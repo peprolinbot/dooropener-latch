46ee4d18d77cb8d5358f92ff105c9dc3578f16b3
Pedro Rey Anca <pedroreya@gmail.com>
2020-06-25 13:27:08 +0200

    Not totally functional. For testing

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-
+import sys
+sys.path.append('latch_sdk_python')
+import config.latch
+import latch
+api = latch.Latch(config.latch.appId, config.latch.secret)
+with open('config/accounts.txt', 'r') as f:
+    account_ids = f.readlines()
+for id in account_ids:
+    response = api.status(id)
+    responseData = str(response.get_data())
+    print(responseData)
