5bffb17f4ddf5057c7d4d2769cb62b2ad0412cc7
Pedro Rey Anca <pedroreya@gmail.com>
2020-06-25 13:37:01 +0200

    Fixed esthetical error

--- accountAdder.py
+++ accountAdder.py
@@ -3,7 +3,7 @@ sys.path.append('latch_sdk_python')
 import config.latch
 import latch
 api = latch.Latch(config.latch.appId, config.latch.secret)
-token = input("Click \"Add Latch\" in the Latch app and give me your pairing code.")
+token = input("Click \"Add Latch\" in the Latch app and give me your pairing code: ")
 response = api.pair(token)
 responseData = str(response.get_data())
 while response.get_data()['accountId'] == None:
