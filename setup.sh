17fc9ebc1e8365a3e65a2832d11969e52c5a5398
Pedro Rey Anca <pedroreya@gmail.com>
2020-06-26 12:38:58 +0200

    Added setup script

---
+++ setup.sh
@@ -0,0 +1,4 @@
+mkdir config
+echo $'appId = "Enter your App Id here with the quotes"\nsecret = "Enter your secret here with the quotes"' > config/latch.py
+touch config/accounts.txt
+touch config/accountsEmails.txt
