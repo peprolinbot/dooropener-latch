b7672780c3faf2f1dfa01ac13d8c1947e4bb09ff
Pedro Rey Anca <pedroreya@gmail.com>
2020-06-26 11:46:01 +0200

    Fully functional incluiding relays. Not tested

--- main.py
+++ main.py
@@ -2,10 +2,34 @@ import sys
 sys.path.append('latch_sdk_python')
 import config.latch
 import latch
+import json
+from time import sleep
+import RPi.GPIO as GPIO
+
+#Setup GPIOP
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(17, GPIO.OUT)
+GPIO.output(17, GPIO.HIGH)
+
+def doorButton():
+    GPIO.output(17, GPIO.LOW)
+    sleep(0.1)
+    GPIO.output(17, GPIO.HIGH)
+
+def openDoor():
+    doorButton()
+    sleep(50)
+    doorButton()
+
 api = latch.Latch(config.latch.appId, config.latch.secret)
 with open('config/accounts.txt', 'r') as f:
-    account_ids = f.readlines()
-for id in account_ids:
-    response = api.status(id)
-    responseData = str(response.get_data())
-    print(responseData)
+    accountIds = [s.replace('\n', '') for s in f.readlines()]
+while True:
+    for id in accountIds:
+        response = api.status(id)
+        responseData = response.get_data()
+        status = responseData['operations'][config.latch.appId]['status']
+        if status == "on":
+            print("Abriendo puerta...")
+            openDoor()
+
