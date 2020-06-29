1aa59dd4f8ef33fcc9f2369243e52ca2e34076a8
Pedro Rey Anca <pedroreya@gmail.com>
2020-06-29 13:36:02 +0200

    Added line for cloning Latch's SDK for Python

--- setup.sh
+++ setup.sh
@@ -2,3 +2,4 @@ mkdir config
 echo $'appId = "Enter your App Id here with the quotes"\nsecret = "Enter your secret here with the quotes"' > config/latch.py
 touch config/accounts.txt
 touch config/accountsEmails.txt
+git clone https://github.com/ElevenPaths/latch-sdk-python.git latch_sdk_python
